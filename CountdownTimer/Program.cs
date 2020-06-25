da9d7d6e967dd667c5cb1c7f87fcf9a491803b1c
Paul Thurmond <paulthurmond@gmail.com>
2020-06-25 08:07:18 -0500

    Add project files.

---
+++ CountdownTimer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountdownTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
